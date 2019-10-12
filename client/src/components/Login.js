f060fb489411c801196a0a74b74c278526ba24fe
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-10-12 09:21:04 -0700

    added utilities and built Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,37 @@
 import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth.js";
 
-const Login = () => {
+const Login = props => {
+  const credentials = { username: "Lambda School", password: "i<3Lambd4" };
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(err => {
+        console.log("Error: ", err);
+      });
+  };
+
   return (
-    <>
+    <div className="login">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <p>Please enter your account information below!</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" placeholder="Lambda School" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" placeholder="i<3Lambd4" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   );
 };
 
